1d90ae1731dac874b33d3a52d472cf2f595f88ce
joao-enrique <almeidadesousajoaoenrique@gmail.com>
2025-11-04 15:14:27 -0300

    💾Feat: add Limiting mouse

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotation_degrees.y -= event.relative.x * 0.3
@@ -7,3 +10,5 @@ func _unhandled_input(event):
 		$Camera3D.rotation_degrees.x = clamp(
 			$Camera3D.rotation_degrees.x, -80.0, 80.0
 		)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
