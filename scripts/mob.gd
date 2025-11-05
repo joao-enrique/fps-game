9d89b7bf9b2ae30f6c68e1fde87df996c8705651
joao-enrique <almeidadesousajoaoenrique@gmail.com>
2025-11-05 14:44:45 -0300

    💾Feat: add Enemy follow player

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,6 +1,15 @@
 extends RigidBody3D
 
+var speed = randf_range(2.0, 4.0)
+
 @onready var bat_model = $bat_model
+@onready var player = get_node("/root/Game/Player")
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	direction.y = 0.0
+	linear_velocity = direction * speed
+	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
 
 func take_damage():
 	bat_model.hurt();
