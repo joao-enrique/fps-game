d8dfa6c94d79df69502b65fe9203e94ba6fb4d81
joao-enrique <almeidadesousajoaoenrique@gmail.com>
2025-11-05 14:59:32 -0300

    💾Feat: add Damaging and killing the mob

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,9 +1,11 @@
 extends RigidBody3D
 
+var health = 3
 var speed = randf_range(2.0, 4.0)
 
 @onready var bat_model = $bat_model
 @onready var player = get_node("/root/Game/Player")
+@onready var timer = $Timer
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
@@ -12,4 +14,18 @@ func _physics_process(delta):
 	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
 
 func take_damage():
+	if health == 0: return
 	bat_model.hurt();
+	health -= 1 
+	
+	if health == 0:
+		set_physics_process(false)
+		gravity_scale = 1.0
+		var direction = -1.0 * global_position.direction_to(player.global_position)
+		var random_upward_force = Vector3.UP * randf_range(1.0, 5.0)
+		apply_central_impulse(direction * 10.0 + random_upward_force)
+		timer.start()
+
+
+func _on_timer_timeout():
+	queue_free()
